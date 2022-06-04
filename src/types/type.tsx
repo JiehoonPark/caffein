10d0fbf0e245c0f2947ffb0c73bb062724e12340
JiehoonPark <z1hoon715@gmail.com>
2022-06-04 18:02:34 +0900

    [feat] add types

---
+++ src/types/type.tsx
@@ -0,0 +1,24 @@
+export type CardType = 'community' | 'camp';
+export interface ICamp {
+  id: number;
+  name: string;
+  status: string;
+  field: string;
+  skill: string;
+  startDate: string;
+  thumbnail: string;
+}
+
+export interface ICommunity {
+  id: number;
+  labels: string[];
+  title: string;
+  content: string;
+  comments: IComment[];
+}
+
+export interface IComment {
+  profile: string;
+  nickname: string;
+  content: string;
+}
