efc777aeb5ea138a4f4dd93c1580e617cd91d3e7
JiehoonPark <96074027+JiehoonPark@users.noreply.github.com>
2022-09-05 23:59:39 +0900

    [refactor] 라우트 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { Route, Routes, Navigate } from "react-router-dom";
 import Nav from "./components/Nav";
-import Home from "./pages/Home";
 import Footer from "./components/Footer";
-import CampDetail from "./pages/CampDetail";
+import Routes from "./routes/Routes";
 import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
@@ -10,11 +8,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/camp/:id" element={<CampDetail />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <Routes />
       <Footer />
     </QueryClientProvider>
   );
